a1681e6dcdd019b1ef1d1c35bc3f6fc4304c9229
orenb99 <77238204+orenb99@users.noreply.github.com>
2022-06-22 03:01:34 +0300

    open and close  navbar

--- native-app/App.js
+++ native-app/App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import { StyleSheet, Text, View, button, Button } from "react-native";
+import { StyleSheet, View, Button, StatusBar } from "react-native";
 import { NativeRouter as Router, Routes, Route } from "react-router-native";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
-import stylesheet from "./stylesheet.json";
 export default function App() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <Router>
-      <Navbar />
+      <StatusBar />
+      <Navbar navOpen={navOpen} />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/register" element={<RegisterPage />} />
       </Routes>
+      <Button title="press me" onPress={() => setNavOpen(!navOpen)} />
     </Router>
   );
 }
-
-const styles = StyleSheet.create(stylesheet);
--- native-app/components/Navbar.js
+++ native-app/components/Navbar.js
@@ -1,36 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Animated } from "react-native";
 import { Link } from "react-router-native";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-const NavBar = () => {
+const NavBar = ({ navOpen }) => {
+  const animatedWidth = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    console.log(navOpen);
+    if (navOpen)
+      Animated.timing(animatedWidth, { toValue: 1, duration: 500 }).start();
+    if (!navOpen)
+      Animated.timing(animatedWidth, { toValue: 0, duration: 500 }).start();
+  }, [navOpen]);
   return (
-    <View style={styles.navbar}>
+    <Animated.View
+      style={{
+        position: "absolute",
+        backgroundColor: "#AAA",
+        height: "100%",
+        width: animatedWidth.interpolate({
+          inputRange: [0, 1],
+          outputRange: ["0%", "30%"],
+        }),
+        overflow: "hidden",
+        zIndex: "2",
+      }}
+    >
       <View style={styles.inner}>
         <Link to="/">
-          <Text>Home</Text>
+          <Text numberOfLines={1}>Home</Text>
         </Link>
         <Link to="/register">
-          <Text>Register</Text>
+          <Text numberOfLines={1}>Register</Text>
         </Link>
         <Link to="/login">
-          <Text>Login</Text>
+          <Text numberOfLines={1}>Login</Text>
         </Link>
       </View>
-    </View>
+    </Animated.View>
   );
 };
 
 export default NavBar;
 
 const styles = StyleSheet.create({
-  navbar: {
-    backgroundColor: "#AAA",
-    padding: 20,
-  },
   inner: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     width: "50%",
     justifyContent: "space-evenly",
+    flexShrink: "1",
   },
 });
