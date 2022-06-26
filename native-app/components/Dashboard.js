0022d988b68b3024fc0965f03279749af6d29c86
orenb99 <77238204+orenb99@users.noreply.github.com>
2022-06-26 15:01:23 +0300

    finished dasshboard

--- native-app/components/Dashboard.js
+++ native-app/components/Dashboard.js
@@ -1,31 +1,99 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableHighlight,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { sendRequest } from "../utils/networkWrapper";
+import Icon from "react-native-vector-icons/AntDesign";
 
 const Dashboard = () => {
   const [userList, setUserList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
-  const [sorting, setSorting] = useState({ key: "name", acc: true });
+  const [sortingKey, setSortingKey] = useState("name");
+  const [sortingAcc, setSortingAcc] = useState(true);
+
   useEffect(() => {
     sendRequest("/admin/users", "get")
       .then(({ data }) => setUserList(data))
       .catch((err) => setErrorMessage(err.response.data));
   }, []);
 
+  const changeOrder = (type) => {
+    if (type === sortingKey) {
+      setSortingAcc(!sortingAcc);
+    } else {
+      setSortingAcc(true);
+      setSortingKey(type);
+    }
+  };
   return (
     <View>
       <Text>Dashboard</Text>
       <FlatList
         data={userList.sort((a, b) =>
-          sorting.acc
-            ? a[sorting.key] > b[sorting.key]
-            : a[sorting.key] < b[sorting.key]
+          sortingAcc
+            ? a[sortingKey] > b[sortingKey]
+            : a[sortingKey] < b[sortingKey]
         )}
         ListHeaderComponent={
           <View style={styles.row}>
-            <Text>name</Text>
-            <Text>role</Text>
-            <Text>joined</Text>
+            <TouchableHighlight onPress={() => changeOrder("name")}>
+              <View
+                style={
+                  sortingKey === "name"
+                    ? styles.titleViewActive
+                    : styles.titleView
+                }
+              >
+                <Text>name</Text>
+                <Icon
+                  name={
+                    sortingKey === "name" && !sortingAcc
+                      ? "caretup"
+                      : "caretdown"
+                  }
+                />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => changeOrder("role")}>
+              <View
+                style={
+                  sortingKey === "role"
+                    ? styles.titleViewActive
+                    : styles.titleView
+                }
+              >
+                <Text>role</Text>
+                <Icon
+                  name={
+                    sortingKey === "role" && !sortingAcc
+                      ? "caretup"
+                      : "caretdown"
+                  }
+                />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => changeOrder("joined")}>
+              <View
+                style={
+                  sortingKey === "joined"
+                    ? styles.titleViewActive
+                    : styles.titleView
+                }
+              >
+                <Text>joined</Text>
+                <Icon
+                  name={
+                    sortingKey === "joined" && !sortingAcc
+                      ? "caretup"
+                      : "caretdown"
+                  }
+                />
+              </View>
+            </TouchableHighlight>
           </View>
         }
         renderItem={({ item }) => (
@@ -49,5 +117,16 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-around",
+    borderBottomWidth: 1,
+  },
+  titleView: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  titleViewActive: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
